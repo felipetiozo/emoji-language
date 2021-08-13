a1e7831cf83641278640ec0863c2de6e2fb00218
Felipe Tiozo <ftiozo@redventures.com>
2021-08-13 01:23:41 -0300

    Working on validator

--- validators/conditions/if.js
+++ validators/conditions/if.js
@@ -1,9 +1,9 @@
 const throwAndExit = require('../../utils/throwAndExit')
 
 module.exports = function(line) {
-  const isValid = /🤡 [a-z]* = [1-9]*/g.test(line)
+  const isValid = /🤔 [a-z]* == ("?[a-z ]*")?|([1-9]*)? { .*}/g.test(line)
 
   if (!isValid) {
-    throwAndExit(`Invalid decimal var line: ${line}`)
+    throwAndExit(`Invalid if condition line: ${line}`)
   }
 }
