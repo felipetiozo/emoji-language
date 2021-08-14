4507a114012fd2226f0cb837c8b5cf66ff106947
Felipe Silva <felipeo.silva@hotmail.com>
2021-08-14 00:15:25 -0300

    Add example file

---
+++ out/index.py
@@ -0,0 +1,21 @@
+text = "hello world"
+number = 2
+number2 = number
+decimal undefined 2.3
+number = 52
+text = "asdasd"
+if text == "hello world":
+	print("ola mundo")
+	
+if number == 2:
+	print("seu numero é igual à 2")
+	
+else:
+	number = 2
+	print("dasdas")
+	
+while number2 < number:
+	number = round(number + 1)
+	
+entrada = input()
+print(entrada)
